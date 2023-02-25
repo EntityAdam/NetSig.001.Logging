42d78b5f803a4fdff9880257caaf515e1bb598f0
for-shariq <forshariq@gmail.com>
2023-02-25 21:49:46 +0530

    init commit

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,8 +1,16 @@
 ﻿using Common;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var hostBuilder = Host.CreateApplicationBuilder(args);
+
+//Get Environment Variable Value
+var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+
+//Load appsettings based on enviroment
+hostBuilder.Configuration.AddJsonFile($"appsettings.{environment}.json");
+
 
 hostBuilder.Services.AddHostedService<Worker>()
                     .AddTransient<IKeyvaultClient, KeyvaultClient>();
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # NetSig.001.Logging
+
+SIG DotNet Refresher
+
+## 1. Declaring and Initializing Bash Variables
+
+```bash
+#Bash - Variables
+rgName="rg-sig-dotnet-refresh"
+keyVaultName="kv-sig-dotnet-refresh"
+keyName="connectionString"
+```
